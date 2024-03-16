58a3776de307bd2d89f30f620ff04bf9b2fc1491
EMA AkTER <joynobhossainema@gmail.com>
2024-03-16 14:33:50 +0600

    updated dashboard

--- src/mainPKG/Marketing_Manager_MainDashboardController.java
+++ src/mainPKG/Marketing_Manager_MainDashboardController.java
@@ -6,7 +6,11 @@ package mainPKG;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
 
 /**
  * FXML Controller class
@@ -15,6 +19,27 @@ import javafx.fxml.Initializable;
  */
 public class Marketing_Manager_MainDashboardController implements Initializable {
 
+    @FXML
+    private VBox goalsVBox;
+    @FXML
+    private Button fundingRequestButton;
+    @FXML
+    private Button analysisOffersButton;
+    @FXML
+    private Button softwareAdvertisementButton;
+    @FXML
+    private Button promotionalAdButton;
+    @FXML
+    private Button equipmentRequestButton;
+    @FXML
+    private Button networkTaskButton;
+    @FXML
+    private Button adminApprovalButton;
+    @FXML
+    private Button emailToSponsorsButton;
+    @FXML
+    private Button logoutButton;
+
     /**
      * Initializes the controller class.
      */
@@ -22,5 +47,41 @@ public class Marketing_Manager_MainDashboardController implements Initializable 
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void handleFundingRequest(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleAnalysisOffers(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleSoftwareAdvertisement(ActionEvent event) {
+    }
+
+    @FXML
+    private void handlePromotionalAd(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleEquipmentRequest(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleNetworkTask(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleAdminApproval(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleEmailToSponsors(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleLogout(ActionEvent event) {
+    }
     
 }
