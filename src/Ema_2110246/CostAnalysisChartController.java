52d85aef8651564502e470177c886a6677bda1a3
EMA AkTER <joynobhossainema@gmail.com>
2024-03-21 14:12:40 +0600

    axx

--- src/Ema_2110246/CostAnalysisChartController.java
+++ src/Ema_2110246/CostAnalysisChartController.java
@@ -6,7 +6,11 @@ package Ema_2110246;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.PieChart;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 /**
  * FXML Controller class
@@ -14,6 +18,13 @@ import javafx.fxml.Initializable;
  * @author ASUS
  */
 public class CostAnalysisChartController implements Initializable {
+
+    @FXML
+    private PieChart viewPieChart;
+    @FXML
+    private Label costAnalysisLabel;
+    @FXML
+    private Button backButton;
 
     /**
      * Initializes the controller class.
