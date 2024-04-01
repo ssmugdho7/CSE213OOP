2bc21c92000b06ea4731bab27a1c53563b5c0856
EMA AkTER <joynobhossainema@gmail.com>
2024-04-02 02:02:20 +0600

    feyvhr

---
+++ src/Ema_2110246/CostAnalysisChartController.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package Ema_2110246;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.chart.PieChart;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+/**
+ * FXML Controller class
+ *
+ * @author ASUS
+ */
+public class CostAnalysisChartController implements Initializable {
+
+    @FXML
+    private Label costAnalysisLabel;
+    @FXML
+    private PieChart viewPieChart;
+    @FXML
+    private Button backButton;
+    @FXML
+    private Label costAnalysisLabel1;
+    @FXML
+    private Button backButton1;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
