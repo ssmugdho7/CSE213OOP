8ae4a9519f8f192e1ad50a8568d7aded57a24389
ssmugdho7 <145373831+ssmugdho7@users.noreply.github.com>
2024-04-07 17:34:17 +0600

    updated
    ggfds

--- src/Jami_2211508/ManagingDirectorDashboardController.java
+++ src/Jami_2211508/ManagingDirectorDashboardController.java
@@ -4,13 +4,19 @@
  */
 package Jami_2211508;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -37,7 +43,20 @@ public class ManagingDirectorDashboardController implements Initializable {
     }
 
     @FXML
-    private void readAuditReportButtonOnClick(ActionEvent event) {
+    private void readAuditReportButtonOnClick(ActionEvent event) throws IOException {
+    
+     FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("ViewAuditReport.fxml"));
+        Parent parent = fxmlLoader.load();
+
+        // Retrieve preferred height and width from the loaded parent node
+        double prefHeight = parent.prefHeight(USE_COMPUTED_SIZE);
+        double prefWidth = parent.prefWidth(USE_COMPUTED_SIZE);
+
+        Stage stage = new Stage();
+        stage.setTitle("Read Audit Report");
+        stage.setScene(new Scene(parent, prefWidth, prefHeight));
+        stage.show();
+    
     }
 
     @FXML
--- src/Mugdho_2220644/Payroll.java
+++ src/Mugdho_2220644/Payroll.java
@@ -4,76 +4,148 @@
  */
 package Mugdho_2220644;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 
 /**
  *
  * @author Lenovo
  */
-public class Payroll extends Employee{ 
-    float salaryAmount, bonusAmount, overtimeAmount,totalSalary;
-    String month; 
-    LocalDate datePaid; 
+public class Payroll extends Employee implements Serializable {
 
-    public Payroll(float salaryAmount, float bonusAmount, float overtimeAmount, String month, LocalDate datePaid, int empId, String empName, String empDesign, String paymentMethod) {
-        super(empId, empName, empDesign, paymentMethod);
-        this.salaryAmount = salaryAmount;
-        this.bonusAmount = bonusAmount;
-        this.overtimeAmount = overtimeAmount;
-        this.month = month;
-        this.datePaid = datePaid;
+
+    public void writeToBinaryFile(ArrayList<Payroll> records) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("payroll.bin"))) {
+            oos.writeObject(records);
+            System.out.println("Records saved to payroll.bin");
+        } catch (IOException e) {
+            e.printStackTrace();
+            showAlert(AlertType.ERROR, "Error", "Failed to save records.", e.getMessage());
+        }
     }
 
+    public ArrayList<Payroll> readFromBinaryFile() {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream("payroll.bin"))) {
+            return (ArrayList<Payroll>) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+            showAlert(AlertType.ERROR, "Error", "Failed to load records.", e.getMessage());
+        }
+        return new ArrayList<>();
+    }
+
+    private void showAlert(AlertType type, String title, String header, String content) {
+        Alert alert = new Alert(type);
+        alert.setTitle(title);
+        alert.setHeaderText(header);
+        alert.setContentText(content);
+        alert.showAndWait();
+    }
     public float getSalaryAmount() {
         return salaryAmount;
-    }
-
-    public float getBonusAmount() {
-        return bonusAmount;
-    }
-
-    public float getOvertimeAmount() {
-        return overtimeAmount;
-    }
-
-    public String getMonth() {
-        return month;
-    }
-
-    public LocalDate getDatePaid() {
-        return datePaid;
     }
 
     public void setSalaryAmount(float salaryAmount) {
         this.salaryAmount = salaryAmount;
     }
 
+    public float getBonusAmount() {
+        return bonusAmount;
+    }
+
     public void setBonusAmount(float bonusAmount) {
         this.bonusAmount = bonusAmount;
+    }
+
+    public float getOvertimeAmount() {
+        return overtimeAmount;
     }
 
     public void setOvertimeAmount(float overtimeAmount) {
         this.overtimeAmount = overtimeAmount;
     }
 
+    public float getTotalSalary() {
+        return totalSalary;
+    }
+
+    public void setTotalSalary(float totalSalary) {
+        this.totalSalary = totalSalary;
+    }
+
+    public String getMonth() {
+        return month;
+    }
+
     public void setMonth(String month) {
         this.month = month;
     }
 
-    public void setDatePaid() {
-        this.datePaid = datePaid;
-    }
-    
-    public float getTotalSalaryAmount(){
-      return totalSalary = this.getSalaryAmount() + this.getBonusAmount() + this.getOvertimeAmount();
+    public LocalDate getDatePaid() {
+        return datePaid;
     }
 
-    @Override
-    public String toString() {
-        return "Payroll{" + "salaryAmount=" + salaryAmount + ", bonusAmount=" + bonusAmount + ", overtimeAmount=" + overtimeAmount + ", totalSalary=" + totalSalary + ", month=" + month + ", datePaid=" + datePaid + '}';
+    public void setDatePaid(LocalDate datePaid) {
+        this.datePaid = datePaid;
     }
-    
-    
-    
-    
+
+    public int getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(int empId) {
+        this.empId = empId;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    public String getEmpDesign() {
+        return empDesign;
+    }
+
+    public void setEmpDesign(String empDesign) {
+        this.empDesign = empDesign;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    float salaryAmount, bonusAmount, overtimeAmount, totalSalary;
+    String month;
+    LocalDate datePaid;
+
+public Payroll(int empId, String empName, String empDesign, String paymentMethod, float salaryAmount, float bonusAmount, float overtimeAmount, String month, LocalDate datePaid) {
+    super(empId, empName, empDesign, paymentMethod);
+    this.salaryAmount = salaryAmount;
+    this.bonusAmount = bonusAmount;
+    this.overtimeAmount = overtimeAmount;
+    this.month = month;
+    this.datePaid = datePaid;
+}
+
+     public Payroll(int empId, String empName, String empDesign, String paymentMethod) {
+        super(empId, empName, empDesign, paymentMethod);
+    }
+
+
+
 }
