c643ca56d9638739af97402d7aef745f2ec5f5d5
EMA AkTER <joynobhossainema@gmail.com>
2024-04-12 15:35:15 +0600

    field con

--- src/Ema_2110246/EmployeeAttendance.java
+++ src/Ema_2110246/EmployeeAttendance.java
@@ -1,61 +1,78 @@
 
 package Ema_2110246;
 
+import java.io.EOFException;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import javafx.collections.ObservableList;
+
 
 
 class EmployeeAttendance implements Serializable {
-    private String desigCol;
-    private float overtimecol;
-    private LocalDate datecol;
-    private String remarksCol;
 
-    public EmployeeAttendance(String desigCol, String remarksCol, Float hoursValue, LocalDate datecol) {
-        this.desigCol = desigCol;
-        this.overtimecol = overtimecol;
-        this.datecol = datecol;
-        this.remarksCol = remarksCol;
+
+    private String designation;
+    private LocalDate date;
+    private int overtimeHours;
+    private String remarks;
+
+    public EmployeeAttendance(String designation, LocalDate date, int overtimeHours, String remarks) {
+        this.designation = designation;
+        this.date = date;
+        this.overtimeHours = overtimeHours;
+        this.remarks = remarks;
     }
 
-    public String getDesigCol() {
-        return desigCol;
+    public String getDesignation() {
+        return designation;
     }
 
-    public void setDesigCol(String desigCol) {
-        this.desigCol = desigCol;
+    public void setDesignation(String designation) {
+        this.designation = designation;
     }
 
-    public float getOvertimecol() {
-        return overtimecol;
+    public LocalDate getDate() {
+        return date;
     }
 
-    public void setOvertimecol(float overtimecol) {
-        this.overtimecol = overtimecol;
+    public void setDate(LocalDate date) {
+        this.date = date;
     }
 
-    public LocalDate getDatecol() {
-        return datecol;
+    public int getOvertimeHours() {
+        return overtimeHours;
     }
 
-    public void setDatecol(LocalDate datecol) {
-        this.datecol = datecol;
+    public void setOvertimeHours(int overtimeHours) {
+        this.overtimeHours = overtimeHours;
     }
 
-    public String getRemarksCol() {
-        return remarksCol;
+    public String getRemarks() {
+        return remarks;
     }
 
-    public void setRemarksCol(String remarksCol) {
-        this.remarksCol = remarksCol;
+    public void setRemarks(String remarks) {
+        this.remarks = remarks;
     }
 
     @Override
     public String toString() {
-        return "EmployeeAttendance{" + "desigCol=" + desigCol + ", overtimecol=" + overtimecol + ", datecol=" + datecol + ", remarksCol=" + remarksCol + '}';
+        return "EmployeeAttendance{" + "designation=" + designation + ", date=" + date + ", overtimeHours=" + overtimeHours + ", remarks=" + remarks + '}';
     }
 
+     
+    }
+     
+     
+     
+    
+    
     
 
-    
-}
--- src/Ema_2110246/EmployeeAttendanceController.java
+++ src/Ema_2110246/EmployeeAttendanceController.java
@@ -4,80 +4,162 @@ package Ema_2110246;
 import java.net.URL;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.util.converter.LocalDateStringConverter;
 
 
 public class EmployeeAttendanceController implements Initializable {
-
     @FXML
-    private ComboBox<String> EmpDesignatioonComboBox;
+    private ComboBox<String> EmployeeDesignatioonComboBox;
     @FXML
-    private DatePicker checkInDateDatepicker;
+    private DatePicker checkInDateDatePicker;
+    @FXML
+    private TableView<EmployeeAttendance> showTableView;
+    @FXML
+    private TableColumn<EmployeeAttendance, String> designationTableColumn;
+    @FXML
+    private TableColumn<EmployeeAttendance, LocalDate> dateTableColumn;
+    @FXML
+    private TableColumn<EmployeeAttendance, Integer> overtimeHourstableColumn;
+    @FXML
+    private TableColumn<EmployeeAttendance, String> remarksTableColumn;
     @FXML
     private TextArea remarksTextArea;
     @FXML
-    private ComboBox<String> overtimeCombobox;
+    private TextField overtimeHoursTextFiled;
     @FXML
-    private TableView<EmployeeAttendance> Tableview;
-    @FXML
-    private TableColumn<EmployeeAttendance, String> desigCol;
-    @FXML
-    private TableColumn<EmployeeAttendance, LocalDate> datecol;
-    @FXML
-    private TableColumn<EmployeeAttendance, Float> overtimecol;
-    @FXML
-    private TableColumn<EmployeeAttendance,String > remarksCol;
-    @FXML
-    private Button BAck;
+    private Button LoadButton;
+    
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        desigCol.setCellValueFactory(new PropertyValueFactory<>("designation"));
-        remarksCol.setCellValueFactory(new PropertyValueFactory<>("remarks"));
-        overtimecol.setCellValueFactory(new PropertyValueFactory<>("hour"));
-        datecol.setCellValueFactory(new PropertyValueFactory<>("date"));
+        designationTableColumn.setCellValueFactory(new PropertyValueFactory<>("designation"));
+        remarksTableColumn.setCellValueFactory(new PropertyValueFactory<>("remarks"));
+        overtimeHourstableColumn.setCellValueFactory(new PropertyValueFactory<>("overtimeHours"));
+        dateTableColumn.setCellValueFactory(new PropertyValueFactory<>("date"));
         
         DateTimeFormatter customDateFormat = DateTimeFormatter.ofPattern("dd MMM yyyy");
-        checkInDateDatepicker.setValue(LocalDate.now());
-        checkInDateDatepicker.setConverter(new LocalDateStringConverter(customDateFormat, null));
+        checkInDateDatePicker.setValue(LocalDate.now());
+        checkInDateDatePicker.setConverter(new LocalDateStringConverter(customDateFormat, null));
         
-        EmpDesignatioonComboBox.getItems().addAll("Marketing Manager", "Field Technician", "Customer Care RePresentative", "Network Engineer",  "Technical Support");
+        EmployeeDesignatioonComboBox.getItems().addAll("Marketing Manager", "Field Technician", "Customer Care RePresentative", "Network Engineer",  "Technical Support");
         
-    }    
+ 
+
+
+       
+        
+    }   
+    
+    
+    
+    @FXML
+    private void loadButtonOnClick(ActionEvent event) {
+        String designation = EmployeeDesignatioonComboBox.getValue();
+        String remarks = remarksTextArea.getText ();
+        String overtimeHours = overtimeHoursTextFiled.getText();
+        int overtimeHoursValue = Integer.parseInt(overtimeHours);
+        LocalDate checkInDate = checkInDateDatePicker.getValue();
+        showTableView.getItems().add(new EmployeeAttendance(designation,checkInDate ,overtimeHoursValue , remarks));
+                
+        
+    }
 
     @FXML
     private void submitButtonOnClick(ActionEvent event) {
+        String designation = EmployeeDesignatioonComboBox.getValue();
+        String remarks = remarksTextArea.getText ();
+        String overtimeHours = overtimeHoursTextFiled.getText();
+        LocalDate checkInDate = checkInDateDatePicker.getValue();
+
+        if (designation == null || remarks == null || overtimeHours.isEmpty() || checkInDate == null) {
+            showErrorAlert("Error", "Please enter all required data.");
+            return;
+        }
+
+        int overtimeHoursValue;
+        try {
+            overtimeHoursValue = Integer.parseInt (overtimeHours);
+        } catch (NumberFormatException e) {
+            showErrorAlert("Error", "Invalid hours format. Please enter a valid number.");
+            return;
+        }
+
+        if (overtimeHoursValue <= 0) {
+            showErrorAlert("Error", "Hours must be a positive value.");
+            return;
+        }
+        
+        
+        List<Object> employeeAttendanceList = MarketingManager.readObjectsFromFile("EmployeeAttendance.bin");
+        for (Object obj : employeeAttendanceList) {
+            if (obj instanceof EmployeeAttendance) {
+                EmployeeAttendance existingRecord = (EmployeeAttendance) obj;
+                if (existingRecord.getDate().equals(checkInDate)) {
+                    showErrorAlert("Error", "Record already submitted for today.");
+                    return;
+                }
+            }
+        }
+            ////////////
+        EmployeeAttendance newEmployeeAttendance = new EmployeeAttendance(designation,checkInDate ,overtimeHoursValue , remarks);
+        //boolean addStatus = MarketingManager.addnewEmployeeAttendance
+        boolean addStatus = MarketingManager.addNewEmployeeAttendance(newEmployeeAttendance, "EmployeeAttendance.bin");
+        if (addStatus) {
+            showInfoAlert("Success", "Send Successfully!");
+        } else {
+            showErrorAlert("Error", "Oops, something went wrong");
+        }
+        
     }
 
-    @FXML
-    private void LoadOnClick(ActionEvent event) {
-        String designation = EmpDesignatioonComboBox.getValue();
-        String remarks = remarksTextArea.getText();
-        String overtime = overtimeCombobox.getValue();
-        Float hoursValue = Float.valueOf(overtime);
-                //valueOf(overtime);
-        LocalDate date = checkInDateDatepicker.getValue();
-        Tableview.getItems().add(new EmployeeAttendance(designation,remarks, hoursValue, date));
+
+
+    /*@FXML
+    private void loadButtonOnClick(ActionEvent event) {
+        String designation = EmployeeDesignatioonComboBox.getValue();
+        String remarks = remarksTextArea.getText ();
+        String overtimeHours = overtimeHoursTextFiled.getText();
+        int overtimeHoursValue = Integer.parseInt(overtimeHours);
+        LocalDate checkInDate = checkInDateDatePicker.getValue();
+        showTableView.getItems().add(new EmployeeAttendance(designation,checkInDate ,overtimeHoursValue , remarks));
+                
+        
+    }*/
+    
+     
+    private void showErrorAlert(String title, String content) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(title);
+        alert.setContentText(content);
+        alert.showAndWait();
     }
 
-    @FXML
-    private void BAckOnClk(ActionEvent event) {
+    private void showInfoAlert(String title, String content) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(title);
+        alert.setContentText(content);
+        alert.showAndWait();
     }
-
     
     
 }
