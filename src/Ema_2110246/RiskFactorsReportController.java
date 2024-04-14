d13f391b1a4ff2b6ce9961100d7744d39c53dcab
EMA AkTER <joynobhossainema@gmail.com>
2024-04-14 10:28:40 +0600

    new upd

--- src/Ema_2110246/RiskFactorsReportController.java
+++ src/Ema_2110246/RiskFactorsReportController.java
@@ -6,6 +6,8 @@ package Ema_2110246;
 
 import java.io.IOException;
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -13,11 +15,13 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
+import javafx.util.converter.LocalDateStringConverter;
 
 /**
  * FXML Controller class
@@ -29,7 +33,7 @@ public class RiskFactorsReportController implements Initializable {
     @FXML
     private TextArea riskFactorDescriptionTextArea;
     @FXML
-    private ComboBox<?> riskCategoryComboBox;
+    private ComboBox<String> riskCategoryComboBox;
     @FXML
     private DatePicker assessmentDateDatePicker;
     @FXML
@@ -45,18 +49,65 @@ public class RiskFactorsReportController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
+        riskCategoryComboBox.getItems().addAll("Financial Risk", "Business Operations Risk","Strategy Troubles","Rule Compliance Risk","Reputation Risk","Other");
+        DateTimeFormatter customDateFormat = DateTimeFormatter.ofPattern("dd MMM yyyy");
+        assessmentDateDatePicker.setValue(LocalDate.now());
+        assessmentDateDatePicker.setConverter(new LocalDateStringConverter(customDateFormat, null));
     }    
 
     @FXML
     private void submitButtonOnClick(ActionEvent event) {
+        
+        try{
+        
+        String riskCategory =riskCategoryComboBox.getValue();
+        String riskFactorDescription = riskFactorDescriptionTextArea.getText();
+        String recommendations = recommendationsTextArea.getText ();
+        LocalDate assessmentDate =assessmentDateDatePicker.getValue();
+        
+        
+   
+   
+   RiskFactors factor = new RiskFactors(riskCategory, riskFactorDescription,  recommendations, assessmentDate);
+  //pushing to model Class done   
+   
+  
+   boolean addStatus =  RiskFactors.addToInstanceToRiskFactors(factor," RiskFactors.bin"); //file writing
+   
+        
+        if (addStatus) {
+                showAlert(Alert.AlertType.INFORMATION, "Success", " RiskFactors Added Successfully!");
+            } else {
+                showAlert(Alert.AlertType.ERROR, "Error", "Oops, something went wrong.");
+            }
+     } 
+         
+         catch (NumberFormatException e) {
+          showAlert(Alert.AlertType.ERROR, "Invalid Input", "Please enter valid  values for riskCategory, riskFactorDescription, recommendations and assessmentDate.");
+        }
     }
+     private void showAlert(Alert.AlertType alertType, String title, String content) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setContentText(content);
+        alert.showAndWait();
 
-    private void BackButtonOnClick(ActionEvent event) {
-        
-        
-        
+    
     }
+    //process = new data 
+    //processes = old data
+     //attendence= new data
+     //attdences= old data
+    //support= new support 
+    // supports= old support
+    
+    //suppt= new support 
+    // suppts= old supp
+     
+     // factor = new factor
+    // factors = old factor
+    
 
     @FXML
     private void backButtonOnClick(ActionEvent event) {
